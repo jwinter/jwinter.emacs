e6b668809e5fd898d3a3cbe67142fd196949f52d
Joe Winter <jwinter@joe-winters-macbook.local>
2008-10-21 21:02:16 -0400

    Move everything into jwinter.el, then require that

---
+++ elisp/jwinter.el
@@ -0,0 +1,163 @@
+(defun prepend-path ( my-path )
+  (setq load-path (cons (expand-file-name my-path) load-path))) 
+(defun append-path ( my-path ) 
+  (setq load-path (append load-path (list (expand-file-name my-path))))) 
+(prepend-path "~/elisp")
+;; Look first in the directory ~/elisp for elisp files (prepend-path "~/elisp")
+;; .emacs
+;;; uncomment this line to disable loading of "default.el" at startup
+;; (setq inhibit-default-init t)
+
+;get rid of menus
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(set-default-font "-Adobe-Courier-Medium-R-Normal--12-120-75-75-M-70-ISO8859-1")
+(if (fboundp 'blink-cursor-mode) (blink-cursor-mode 0))
+
+(global-set-key [delete] 'delete-char)
+(global-set-key [kp-delete] 'delete-char)
+(global-set-key "\C-xt" 'beginning-of-buffer)
+(global-set-key "\M-g" 'goto-line)
+(global-set-key [f5]    'call-last-kbd-macro)
+
+(setq tramp-default-method "ssh")
+
+;;Replace ^M with newlines
+(defun mac-newlines ()
+  (interactive)
+  (replace-string "" "\n"))
+
+
+;; insert a pdb.set_trace() at point (look into defining this only for python)
+(defun insert_trace ()
+  "insert pdb trace at point"
+  (interactive)
+  (save-excursion
+    (insert "import pdb;pdb.set_trace();")
+    ))
+(global-set-key "\C-ci" 'insert_trace)
+
+
+;; Stevey's Javascript mode: http://code.google.com/p/js2-mode/
+(autoload 'js2-mode "js2" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
+
+;; turn on font-lock mode
+(global-font-lock-mode t)
+;; enable visual feedback on selections
+(setq-default transient-mark-mode t)
+
+;; Put autosave files (ie #foo#) in one place, *not* scattered all over the
+;; file system! (The make-autosave-file-name function is invoked to determine
+;; the filename of an autosave file.)
+(defvar autosave-dir (concat "/tmp/emacs_autosaves/" (user-login-name) "/"))
+(make-directory autosave-dir t)
+
+(defun auto-save-file-name-p (filename) (string-match "^#.*#$" (file-name-nondirectory filename)))
+
+(defun make-auto-save-file-name ()
+  (concat autosave-dir (if buffer-file-name (concat "#" (file-name-nondirectory buffer-file-name) "#")
+                         (expand-file-name (concat "#%" (buffer-name) "#")))))
+
+;; Put backup files (ie foo~) in one place too. (The backup-directory-alist
+;; list contains regexp=>directory mappings; filenames matching a regexp are
+;; backed up in the corresponding directory. Emacs will mkdir it if necessary.)
+(defvar backup-dir (concat "/tmp/emacs_backups/" (user-login-name) "/")) (setq backup-directory-alist (list (cons "." backup-dir)))
+
+
+
+;; always end a file with a newline
+(setq require-final-newline t)
+
+;; stop at the end of the file, not just add lines
+(setq next-line-add-newlines nil)
+
+(when window-system
+  ;; enable wheelmouse support by default
+  (mwheel-install)
+  ;; use extended compound-text coding for X clipboard
+  (set-selection-coding-system 'compound-text-with-extensions))
+
+(server-start)  ; Starts server for (among others) emacsclient
+
+(load-library "css-mode")
+(setq auto-mode-alist       
+      (cons '("\\.css\\'" . css-mode) auto-mode-alist))
+(setq cssm-indent-function #'cssm-c-style-indenter)
+
+;; enable visual feedback on selections
+;(setq transient-mark-mode t)
+
+;; default to better frame titles
+(setq frame-title-format
+      (concat  "%b - emacs@" system-name))
+
+(require 'ido)
+(ido-mode t)
+
+;; I-search with initial contents
+(defvar isearch-initial-string nil)
+
+(defun isearch-set-initial-string ()
+  (remove-hook 'isearch-mode-hook 'isearch-set-initial-string)
+  (setq isearch-string isearch-initial-string)
+  (isearch-search-and-update))
+
+(defun isearch-forward-at-point (&optional regexp-p no-recursive-edit)
+  "Interactive search forward for the symbol at point."
+  (interactive "P\np")
+  (if regexp-p (isearch-forward regexp-p no-recursive-edit)
+    (let* ((end (progn (skip-syntax-forward "w_") (point)))
+           (begin (progn (skip-syntax-backward "w_") (point))))
+      (if (eq begin end)
+          (isearch-forward regexp-p no-recursive-edit)
+        (setq isearch-initial-string (buffer-substring begin end))
+        (add-hook 'isearch-mode-hook 'isearch-set-initial-string)
+        (isearch-forward regexp-p no-recursive-edit)))))
+(global-set-key "\C-s" 'isearch-forward)
+(global-set-key [?\C-\S-S] 'isearch-forward-at-point)
+(load-library "vc-svn")
+
+
+;; http://steve.yegge.googlepages.com/my-dot-emacs-file
+;; Never understood why Emacs doesn't have this function.
+;;
+(defun rename-file-and-buffer (new-name)
+ "Renames both current buffer and file it's visiting to NEW-NAME." (interactive "sNew name: ")
+ (let ((name (buffer-name))
+	(filename (buffer-file-name)))
+ (if (not filename)
+	(message "Buffer '%s' is not visiting a file!" name)
+ (if (get-buffer new-name)
+	 (message "A buffer named '%s' already exists!" new-name)
+	(progn 	 (rename-file name new-name 1) 	 (rename-buffer new-name) 	 (set-visited-file-name new-name) 	 (set-buffer-modified-p nil)))))) ;;
+;; Never understood why Emacs doesn't have this function, either.
+;;
+(defun move-buffer-file (dir)
+ "Moves both current buffer and file it's visiting to DIR." (interactive "DNew directory: ")
+ (let* ((name (buffer-name))
+	 (filename (buffer-file-name))
+	 (dir
+	 (if (string-match dir "\\(?:/\\|\\\\)$")
+	 (substring dir 0 -1) dir))
+	 (newname (concat dir "/" name)))
+
+   (if (not filename)
+       (message "Buffer '%s' is not visiting a file!" name)
+     (progn 	(copy-file filename newname 1) 	(delete-file filename) 	(set-visited-file-name newname) 	(set-buffer-modified-p nil) 	t)))) 
+
+
+(load-library "haskell-mode-2.4/haskell-site-file")
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)  
+;; Rinari
+(append-path "~/elisp/rinari")
+(require 'rinari)
+
+
+;; Better python mode http://sourceforge.net/projects/python-mode/
+(setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
+(setq interpreter-mode-alist (cons '("python" . python-mode)
+                                   interpreter-mode-alist))
+(autoload 'python-mode "python-mode" "Python editing mode." t)
